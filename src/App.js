48d50aa04862f0a8dce7a27506d742f37fe906e4
GiselaFlores <gi.flores@bue.edu.ar>
2023-10-11 11:53:36 -0300

    inicio context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import User from "./componentes/User";
+
+export const UserContext = React.createContext();
 
 function App() {
+
+  const user = {
+    nombre: "Gisela",
+    edad:41,
+    sector: "codo a codo",
+    cargo: "docente"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <UserContext.Provider value={user}>
+       
+       <User/>
+     
+    </UserContext.Provider>
   );
 }
 
